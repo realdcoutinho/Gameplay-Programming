8d0b3f45932317bccc59a9105f2428f16bc036e4
realdcoutinho <diogo.de.moura.coutinho@student.howest.be>
2022-10-29 13:07:51 +0200

    update

---
+++ Week 5/AgentPath/source/projects/App_Selector.h
@@ -0,0 +1,50 @@
+#pragma once
+// Here we determine which application is currently active
+// Create the define here using the "ActiveApp_..." convention and set up the correct include and typedef in the #ifdef below.
+
+//#define ActiveApp_Sandbox
+//#define ActiveApp_Steering
+//#define ActiveApp_CombinedSteering
+//#define ActiveApp_Flocking
+//#define ActiveApp_GraphTheory
+#define ActiveApp_PathfindingAStar
+
+//---------- Registered Applications -----------
+#ifdef ActiveApp_Sandbox
+#include "projects/Movement/Sandbox/App_Sandbox.h"
+typedef App_Sandbox CurrentApp;
+#endif ActiveApp_Sandbox
+
+#ifdef ActiveApp_Steering
+#include "projects/Movement/SteeringBehaviors/Steering/App_SteeringBehaviors.h"
+typedef App_SteeringBehaviors CurrentApp;
+#endif ActiveApp_Steering
+
+#ifdef ActiveApp_CombinedSteering
+#include "projects/Movement/SteeringBehaviors/CombinedSteering/App_CombinedSteering.h"
+typedef App_CombinedSteering CurrentApp;
+#endif ActiveApp_CombinedSteering
+
+#ifdef ActiveApp_Flocking
+#include "projects/Movement/SteeringBehaviors/Flocking/App_Flocking.h"
+typedef App_Flocking CurrentApp;
+#endif ActiveApp_Flocking
+
+
+#ifdef ActiveApp_GraphTheory
+#include "projects/Movement/Pathfinding/GraphTheory/App_GraphTheory.h"
+typedef App_GraphTheory CurrentApp;
+#endif ActiveApp_GraphTheory
+
+#ifdef ActiveApp_PathfindingAStar
+#include "projects/Movement/Pathfinding/AStar/App_PathfindingAStar/App_PathfindingAStar.h"
+typedef App_PathfindingAStar CurrentApp;
+#endif ActiveApp_PathfindingAStar
+
+class App_Selector {
+public: 
+	static IApp* CreateApp() {
+		IApp* myApp = new CurrentApp();
+		return myApp;
+	}
+};
